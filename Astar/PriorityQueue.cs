79db340132282eead74db57f86679f14d3f87aab
Ann <a.trembitskaya@mail.ru>
2016-01-28 15:47:04 +0300

    PriorityQueue: SortedDic -> Heap
    Optimized complexity

--- Astar/PriorityQueue.cs
+++ Astar/PriorityQueue.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Astar {
-    class PriorityQueue<P, V> {
+   /* class PriorityQueue<P, V>
+    {
         private SortedDictionary<P, Queue<V>> dict = new SortedDictionary<P, Queue<V>>();
 
         public void Enqueue(P priority, V value) {
@@ -29,5 +30,66 @@ namespace Astar {
         public bool IsEmpty {
             get { return !dict.Any(); }
         }
-    }
+    } */
+    class PriorotyQueue<P, V> where P : IComparable
+        {
+            struct Node {
+                public P priority;
+                public V value;
+            }
+            private List<Node> heap = new List<Node>(); // List<T> внутри массив. 
+        //это Generic эквивалент ArrayList, который представляет собой массив с динамическим изменением размера по требованию
+
+            private void Enqueue(P priority, V value) {  
+                var index = heap.Count();
+                var parentIndex = (index - 1) / 2;
+                heap.Add(new Node { priority = priority, value = value });
+                while (heap[parentIndex].priority.CompareTo(heap[index].priority) > 0 && index > 0) {
+                    var t = heap[parentIndex];
+                    heap[parentIndex] = heap[index];
+                    heap[index] = t;
+                    index = parentIndex;
+                    parentIndex = (index - 1) / 2;
+                }
+
+            }
+
+            private V Dequeue() {
+                var lastIndex = heap.Count();
+                var result = heap[0].value;
+                heap[0] = heap[lastIndex - 1];
+                heap.RemoveAt(lastIndex-1);
+
+                Heapify(0);
+                return result;
+            }
+
+            public bool IsEmpty
+            {
+                get { return heap.Count==0; }
+            }
+
+            public void Heapify(int index) {
+                var smallestChildIndex = index;
+                var leftChildIndex = 2 * index + 1;
+                var rightChildIndex = 2 * index + 2;
+
+                if (leftChildIndex < heap.Count &&
+                    heap[leftChildIndex].priority.CompareTo(heap[index].priority) < 0) {
+                    smallestChildIndex = leftChildIndex;
+                }
+
+                if (rightChildIndex * heap.Count >0 &&
+                    heap[leftChildIndex].priority.CompareTo(heap[index].priority) < 0) {
+                    smallestChildIndex = rightChildIndex;
+                }
+
+                if (!(smallestChildIndex == index)) {
+                    var t = heap[smallestChildIndex];
+                    heap[smallestChildIndex] = heap[index];
+                    heap[index] = t;
+                }
+            }
+
+        }
 }
